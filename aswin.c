81fb1017e6f14461457ff89a42ca8e771d16f42c
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-01-25 16:55:41 +0530

    Create aswin.c

---
+++ aswin.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+void main()
+{
+    int num;
+    printf ("enter the num=3");
+    scanf("%d",& num);
+    {
+        if(num>0)
+        printf("number is positive");
+        else if(num<0)
+        printf ("number is negative");
+        else
+        printf ("number is zero");
+                                                  
+    }
+}
