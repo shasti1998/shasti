612397bf266e83fdf779b05fd5210582adb936de
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-01-26 12:03:24 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include<conio.h>
+{
+    int a;
+    printf("enter the numbera");
+    scanf("%d &a");
+    if(a%2==0)
+    {
+        printf("The number is even number");
+        else
+        printf("The number is odd number");
+    }
+}
